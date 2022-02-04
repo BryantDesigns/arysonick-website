12bf703f3342354c779833367b5ad7a8ca503468
Tyler Bryant <tbryantdesignsllc@gmail.com>
2022-02-04 07:33:43 -0700

    Added more styling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import Feature from './components/Feature'
 import Footer from './components/Footer';
 import Form from './components/Form';
+import bg from './images/dj1.jpg'
 
 function App() {
   
   return (
     <>
-      <Feature />
+      <div style={{ background: `url(${bg}) center center cover`}}>
+        <Feature />
+      </div>
+      
       <Form />
       <Footer />
     </>
--- src/components/Feature.jsx
+++ src/components/Feature.jsx
@@ -1,13 +1,13 @@
 import NickImage from '../images/nick.jpg'
 
 
-export default function Example() {
+export default function Feature() {
   return (
-    <div className="relative bg-white py-16 sm:py-24">
-      <div className="lg:mx-auto lg:max-w-7xl lg:px-8 lg:grid lg:grid-cols-2 lg:gap-24 lg:items-start">
+    <div className="relative py-16 sm:py-24">
+      <div className="lg:mx-auto lg:max-w-7xl lg:px-8 lg:grid lg:grid-cols-2 lg:gap-24 lg:items-start bg-white bg-opacity-60">
         <div className="relative sm:py-16 lg:py-0">
           <div aria-hidden="true" className="hidden sm:block lg:absolute lg:inset-y-0 lg:right-0 lg:w-screen">
-            <div className="absolute inset-y-0 right-1/2 w-full bg-gray-50 rounded-r-3xl lg:right-72" />
+            <div className="absolute inset-y-0 right-1/2 w-full rounded-r-3xl lg:right-72" />
             <svg
               className="absolute top-8 left-1/2 -ml-3 lg:-right-8 lg:left-auto lg:top-12"
               width={404}
@@ -24,7 +24,7 @@ export default function Example() {
                   height={20}
                   patternUnits="userSpaceOnUse"
                 >
-                  <rect x={0} y={0} width={4} height={4} className="text-gray-200" fill="currentColor" />
+                  <rect x={0} y={0} width={4} height={4} className="text-orange-500" fill="currentColor" />
                 </pattern>
               </defs>
               <rect width={404} height={392} fill="url(#02f20b47-fd69-4224-a62a-4c9de5c763f7)" />
@@ -51,11 +51,11 @@ export default function Example() {
 
         <div className="relative mx-auto max-w-md px-4 sm:max-w-3xl sm:px-6 lg:px-0">
           {/* Content area */}
-          <div className="pt-12 sm:pt-16 lg:pt-20">
+          <div className="py-12 sm:py-16 lg:py-20">
             <h2 className="text-3xl text-gray-900 font-extrabold tracking-tight sm:text-4xl">
               ARYSON!CK
             </h2>
-            <div className="mt-6 text-gray-500 space-y-6">
+            <div className="my-6 text-gray-900 space-y-6 font-medium">
               <p className="text-base leading-7">
                 Nick Santangelo who goes by the stage name ARYSON!CK, is an up and coming EDM DJ/Producer. He is currently attending the Recording Radio Film Connection School, and will be receiving his certificates in Ableton production and audio engineering. Born in Albuquerque, New Mexico ARYSON!CK got into producing Hip-Hop music when he was sixteen as a hobby. Over the years that hobby turned into a passion and love for music.
               </p>
