634740d8aab0b36f08b693a56513a8d2ecc7c110
Tyler Bryant <tbryantdesignsllc@gmail.com>
2022-02-03 21:36:04 -0700

    Fixed feature

--- src/components/Feature.jsx
+++ src/components/Feature.jsx
@@ -1,100 +1,70 @@
 import NickImage from '../images/nick.jpg'
+
+
 export default function Example() {
   return (
-    <div className="py-16 bg-gray-50 overflow-hidden lg:py-24">
-      <div className="relative max-w-xl mx-auto px-4 sm:px-6 lg:px-8 lg:max-w-7xl">
-        <svg
-          className="hidden lg:block absolute left-full transform -translate-x-1/2 -translate-y-1/4"
-          width={404}
-          height={784}
-          fill="none"
-          viewBox="0 0 404 784"
-          aria-hidden="true"
-        >
-          <defs>
-            <pattern
-              id="b1e6e422-73f8-40a6-b5d9-c8586e37e0e7"
-              x={0}
-              y={0}
-              width={20}
-              height={20}
-              patternUnits="userSpaceOnUse"
+    <div className="relative bg-white py-16 sm:py-24">
+      <div className="lg:mx-auto lg:max-w-7xl lg:px-8 lg:grid lg:grid-cols-2 lg:gap-24 lg:items-start">
+        <div className="relative sm:py-16 lg:py-0">
+          <div aria-hidden="true" className="hidden sm:block lg:absolute lg:inset-y-0 lg:right-0 lg:w-screen">
+            <div className="absolute inset-y-0 right-1/2 w-full bg-gray-50 rounded-r-3xl lg:right-72" />
+            <svg
+              className="absolute top-8 left-1/2 -ml-3 lg:-right-8 lg:left-auto lg:top-12"
+              width={404}
+              height={392}
+              fill="none"
+              viewBox="0 0 404 392"
             >
-              <rect x={0} y={0} width={4} height={4} className="text-gray-200" fill="currentColor" />
-            </pattern>
-          </defs>
-          <rect width={404} height={784} fill="url(#b1e6e422-73f8-40a6-b5d9-c8586e37e0e7)" />
-        </svg>
-
-        <div className="relative">
-          <h1 className="text-center text-4xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
-            ARYSON!CK
-          </h1>
-        </div>
-
-        
-
-        <svg
-          className="hidden lg:block absolute right-full transform translate-x-1/2 translate-y-12"
-          width={404}
-          height={784}
-          fill="none"
-          viewBox="0 0 404 784"
-          aria-hidden="true"
-        >
-          <defs>
-            <pattern
-              id="64e643ad-2176-4f86-b3d7-f2c5da3b6a6d"
-              x={0}
-              y={0}
-              width={20}
-              height={20}
-              patternUnits="userSpaceOnUse"
-            >
-              <rect x={0} y={0} width={4} height={4} className="text-gray-200" fill="currentColor" />
-            </pattern>
-          </defs>
-          <rect width={404} height={784} fill="url(#64e643ad-2176-4f86-b3d7-f2c5da3b6a6d)" />
-        </svg>
-
-        <div className="relative mt-12 sm:mt-16 lg:mt-24">
-          <div className="lg:grid lg:grid-flow-row-dense lg:grid-cols-2 lg:gap-8 lg:items-center">
-            <div className="lg:col-start-2">
-              <h3 className="text-2xl font-extrabold text-gray-900 tracking-tight sm:text-3xl">Who Am I?</h3>
-              <p className="mt-3 text-lg text-gray-500">
-                Nick Santangelo who goes by the stage name ARYSON!CK, is an up and coming EDM DJ/Producer. He is currently attending the Recording Radio Film Connection School, and will be receiving his certificates in Ableton production and audio engineering. Born in Albuquerque, New Mexico ARYSON!CK got into producing Hip-Hop music when he was sixteen as a hobby. Over the years that hobby turned into a passion and love for music. In the Summer of 2015 ARYSON!CK went to Las Vegas, NV for a trip, it was then, where he saw Calvin Harris and got inspired to start DJing. ARYSONICK started DJing graduation parties, events, and weddings on the side. ARYSON!CK wanted to take his passion and love for music production and DJing to the next level. That's why he is currently attending Recording Radio Film Connection.
-              </p>
-            </div>
-
-            <div className="mt-10 -mx-4 relative lg:mt-0 lg:col-start-1">
-              <svg
-                className="absolute left-1/2 transform -translate-x-1/2 translate-y-16 lg:hidden"
-                width={784}
-                height={404}
-                fill="none"
-                viewBox="0 0 784 404"
-                aria-hidden="true"
-              >
-                <defs>
-                  <pattern
-                    id="e80155a9-dfde-425a-b5ea-1f6fadd20131"
-                    x={0}
-                    y={0}
-                    width={20}
-                    height={20}
-                    patternUnits="userSpaceOnUse"
-                  >
-                    <rect x={0} y={0} width={4} height={4} className="text-gray-200" fill="currentColor" />
-                  </pattern>
-                </defs>
-                <rect width={784} height={404} fill="url(#e80155a9-dfde-425a-b5ea-1f6fadd20131)" />
-              </svg>
+              <defs>
+                <pattern
+                  id="02f20b47-fd69-4224-a62a-4c9de5c763f7"
+                  x={0}
+                  y={0}
+                  width={20}
+                  height={20}
+                  patternUnits="userSpaceOnUse"
+                >
+                  <rect x={0} y={0} width={4} height={4} className="text-gray-200" fill="currentColor" />
+                </pattern>
+              </defs>
+              <rect width={404} height={392} fill="url(#02f20b47-fd69-4224-a62a-4c9de5c763f7)" />
+            </svg>
+          </div>
+          <div className="relative mx-auto max-w-md px-4 sm:max-w-3xl sm:px-6 lg:px-0 lg:max-w-none lg:py-20">
+            {/* Testimonial card*/}
+            <div className="relative pt-64 pb-10 rounded-2xl shadow-xl overflow-hidden">
               <img
-                className="relative mx-auto"
-                width={590}
+                className="absolute inset-0 h-full w-full object-cover"
                 src={NickImage}
                 alt=""
               />
+              <div className="absolute inset-0 " />
+              <div className="absolute inset-0" />
+              <div className="relative px-8">
+                
+                  
+                </div>
+                
+            </div>
+          </div>
+        </div>
+
+        <div className="relative mx-auto max-w-md px-4 sm:max-w-3xl sm:px-6 lg:px-0">
+          {/* Content area */}
+          <div className="pt-12 sm:pt-16 lg:pt-20">
+            <h2 className="text-3xl text-gray-900 font-extrabold tracking-tight sm:text-4xl">
+              ARYSON!CK
+            </h2>
+            <div className="mt-6 text-gray-500 space-y-6">
+              <p className="text-lg">
+                Nick Santangelo who goes by the stage name ARYSON!CK, is an up and coming EDM DJ/Producer. He is currently attending the Recording Radio Film Connection School, and will be receiving his certificates in Ableton production and audio engineering. Born in Albuquerque, New Mexico ARYSON!CK got into producing Hip-Hop music when he was sixteen as a hobby. Over the years that hobby turned into a passion and love for music.
+              </p>
+              <p className="text-base leading-7">
+                In the Summer of 2015 ARYSON!CK went to Las Vegas, NV for a trip, it was then, where he saw Calvin Harris and got inspired to start DJing. ARYSONICK started DJing graduation parties, events, and weddings on the side.
+              </p>
+              <p className="text-base leading-7">
+                ARYSON!CK wanted to take his passion and love for music production and DJing to the next level. That's why he is currently attending Recording Radio Film Connection.
+              </p>
             </div>
           </div>
         </div>
@@ -102,3 +72,4 @@ export default function Example() {
     </div>
   )
 }
+
