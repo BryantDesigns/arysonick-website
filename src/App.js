2664666aa29c9f15d54fe414fb71b95b951e9020
Tyler Bryant <tbryantdesignsllc@gmail.com>
2022-02-05 21:10:06 -0700

    Change design

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
-import Feature from './components/Feature'
+import Feature from './components/Feature';
 import Footer from './components/Footer';
-import Form from './components/Form';
+import HeaderImage from './images/dj1.jpg';
 
 function App() {
-  
   return (
     <>
-      
-        <Feature />
-     
-      
-      <Form />
-      <Footer />
+      <div className='bg-zinc-900 h-screen'>
+        <div
+          className='h-96 flex justify-center items-center'
+          style={{
+            backgroundImage: `url(${HeaderImage})`,
+            backgroundSize: 'cover',
+            backgroundPosition: 'center center',
+            backgroundRepeat: 'no-repeat',
+          }}
+        >
+          <h1 className='text-8xl -mt-20 md:text-9xl text-orange-500 drop-shadow-lg '>
+            ARYSON!CK
+          </h1>
+        </div>
+        <div className='flex-col mx-8 -mt-20 justify-between bg-gradient-to-t from-zinc-800 to-zinc-700 rounded-xl shadow-xl shadow-orange-500 text-gray-400'>
+          <Feature />
+          <Footer />
+        </div>
+      </div>
     </>
   );
 }
--- src/components/Feature.jsx
+++ src/components/Feature.jsx
@@ -1,76 +1,102 @@
-import NickImage from '../images/nick.jpg'
-import bg from '../images/dj1.jpg'
-
+import NickImage from '../images/nick.jpg';
 
 export default function Feature() {
   return (
-    <div className="relative py-16 sm:py-24" style={{ background: `url(${bg}) center center`}}>
-      <div className="lg:mx-auto lg:max-w-7xl lg:px-8 lg:grid lg:grid-cols-2 lg:gap-24 lg:items-start bg-white bg-opacity-60">
-        <div className="relative sm:py-16 lg:py-0">
-          <div aria-hidden="true" className="hidden sm:block lg:absolute lg:inset-y-0 lg:right-0 lg:w-screen">
-            <div className="absolute inset-y-0 right-1/2 w-full rounded-r-3xl lg:right-72" />
+    <div className='relative py-16 sm:py-24'>
+      <div className='lg:mx-auto lg:max-w-7xl lg:px-8 lg:grid lg:grid-cols-2 lg:gap-24 lg:items-start'>
+        <div className='relative sm:py-16 lg:py-0'>
+          <div
+            aria-hidden='true'
+            className='hidden sm:block lg:absolute lg:inset-y-0 lg:right-0 lg:w-screen'
+          >
+            <div className='absolute inset-y-0 right-1/2 w-full rounded-r-3xl lg:right-72' />
+
             <svg
-              className="absolute top-8 left-1/2 -ml-3 lg:-right-8 lg:left-auto lg:top-12"
+              className='absolute top-8 left-1/2 -ml-3 lg:-right-8 lg:left-auto lg:top-12'
               width={404}
               height={392}
-              fill="none"
-              viewBox="0 0 404 392"
+              fill='none'
+              viewBox='0 0 404 392'
             >
               <defs>
                 <pattern
-                  id="02f20b47-fd69-4224-a62a-4c9de5c763f7"
+                  id='02f20b47-fd69-4224-a62a-4c9de5c763f7'
                   x={0}
                   y={0}
                   width={20}
                   height={20}
-                  patternUnits="userSpaceOnUse"
+                  patternUnits='userSpaceOnUse'
                 >
-                  <rect x={0} y={0} width={4} height={4} className="text-orange-500" fill="currentColor" />
+                  <rect
+                    x={0}
+                    y={0}
+                    width={4}
+                    height={4}
+                    className='text-orange-500'
+                    fill='currentColor'
+                  />
                 </pattern>
               </defs>
-              <rect width={404} height={392} fill="url(#02f20b47-fd69-4224-a62a-4c9de5c763f7)" />
+              <rect
+                width={404}
+                height={392}
+                fill='url(#02f20b47-fd69-4224-a62a-4c9de5c763f7)'
+              />
             </svg>
           </div>
-          <div className="relative mx-auto max-w-md px-4 sm:max-w-3xl sm:px-6 lg:px-0 lg:max-w-none lg:py-20">
+          <div className='relative mx-auto max-w-md px-4 sm:max-w-3xl sm:px-6 lg:px-0 lg:max-w-none lg:py-20'>
             {/* Testimonial card*/}
-            <div className="relative pt-64 pb-10 rounded-2xl shadow-xl overflow-hidden">
+            <div className='relative h-96 pt-64 pb-10 rounded-2xl shadow-xl overflow-hidden'>
               <img
-                className="absolute inset-0 h-full w-full object-cover"
+                className='absolute inset-0 h-full w-full object-cover'
                 src={NickImage}
-                alt=""
+                alt=''
               />
-              <div className="absolute inset-0 " />
-              <div className="absolute inset-0" />
-              <div className="relative px-8">
-                
-                  
-                </div>
-                
+              <div className='absolute inset-0 ' />
+              <div className='absolute inset-0' />
+              <div className='relative px-8'></div>
             </div>
           </div>
         </div>
 
-        <div className="relative mx-auto max-w-md px-4 sm:max-w-3xl sm:px-6 lg:px-0">
+        <div className='relative mx-auto max-w-md px-4 sm:max-w-3xl sm:px-6 lg:px-0'>
           {/* Content area */}
-          <div className="py-12 sm:py-16 lg:py-20">
-            <h2 className="text-3xl text-gray-900 font-extrabold tracking-tight sm:text-4xl">
-              ARYSON!CK
+          <div className='py-12 sm:py-16 lg:py-20 text-left'>
+            <h2 className='text-3xl font-extrabold tracking-tight sm:text-4xl capitalize'>
+              A little about me
             </h2>
-            <div className="my-6 text-gray-900 space-y-6 font-medium">
-              <p className="text-base leading-7">
-                Nick Santangelo who goes by the stage name ARYSON!CK, is an up and coming EDM DJ/Producer. He is currently attending the Recording Radio Film Connection School, and will be receiving his certificates in Ableton production and audio engineering. Born in Albuquerque, New Mexico ARYSON!CK got into producing Hip-Hop music when he was sixteen as a hobby. Over the years that hobby turned into a passion and love for music.
+            <div className='my-6 space-y-6 font-medium'>
+              <p className='text-base leading-7'>
+                Nick Santangelo who goes by the stage name{' '}
+                <span className='font-bold text-orange-500'>ARYSON!CK</span>, is
+                an up and coming EDM DJ/Producer. He is currently attending the
+                Recording Radio Film Connection School, and will be receiving
+                his certificates in Ableton production and audio engineering.
+                Born in Albuquerque, New Mexico{' '}
+                <span className='font-bold text-orange-500'>ARYSON!CK</span> got
+                into producing Hip-Hop music when he was sixteen as a hobby.
+                Over the years that hobby turned into a passion and love for
+                music.
               </p>
-              <p className="text-base leading-7">
-                In the Summer of 2015 ARYSON!CK went to Las Vegas, NV for a trip, it was then, where he saw Calvin Harris and got inspired to start DJing. ARYSONICK started DJing graduation parties, events, and weddings on the side.
+              <p className='text-base leading-7'>
+                In the Summer of 2015{' '}
+                <span className='font-bold text-orange-500'>ARYSON!CK</span>{' '}
+                went to Las Vegas, NV for a trip, it was then, where he saw
+                Calvin Harris and got inspired to start DJing.{' '}
+                <span className='font-bold text-orange-500'>ARYSON!CK</span>{' '}
+                started DJing graduation parties, events, and weddings on the
+                side.
               </p>
-              <p className="text-base leading-7">
-                ARYSON!CK wanted to take his passion and love for music production and DJing to the next level. That's why he is currently attending Recording Radio Film Connection.
+              <p className='text-base leading-7'>
+                <span className='font-bold text-orange-500'>ARYSON!CK</span>{' '}
+                wanted to take his passion and love for music production and
+                DJing to the next level. That's why he is currently attending
+                Recording Radio Film Connection.
               </p>
             </div>
           </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
-
